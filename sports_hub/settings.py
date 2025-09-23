aef2cbe7b545bd904441a4de5f455e4ec0d2b6b4
BintoroNtw <bintoronatawijaya@gmail.com>
2025-09-23 20:03:36 +0700

    Fixing forbidden error 403

--- sports_hub/settings.py
+++ sports_hub/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost","127.0.0.1","bintoro-nata-sportshub.pbp.cs.ui.ac.id"]
 ...
 CSRF_TRUSTED_ORIGINS = [
-    "https://bintoro-nata-sportshub.pbp.cs.ui.ac.id/",
+    "https://bintoro-nata-sportshub.pbp.cs.ui.ac.id",
 ]
 ...
 
